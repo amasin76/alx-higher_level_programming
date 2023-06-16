d354f249b0e6fbc9eb85f11149d3d49860a43b4a
amasin76 <amasin76@gmail.com>
2023-06-16 07:34:19 +0000

    Fix: typo

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result, prev_value = 0
+    result, prev_value = 0, 0
     for c in roman_string:
         value = roman_d[c]
         result += value
@@ -11,3 +11,19 @@ def roman_to_int(roman_string):
             result -= prev_value * 2
         prev_value = value
     return result
+
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
