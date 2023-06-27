69873ba62993f6485004954fbb657fec843c98fb
amasin76 <amasin76@gmail.com>
2023-06-27 13:05:50 +0000

    Add: `docstring` comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """A class to represent a square object"""
+
     def __init__(self, size):
+        """Initialize the size of the square"""
         self.__size = size
