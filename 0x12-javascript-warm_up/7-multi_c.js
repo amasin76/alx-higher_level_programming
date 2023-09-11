b76d43fd51fe19dd2e3962dbedee5f6a36d302b6
amasin76 <amasin76@gmail.com>
2023-09-11 23:43:19 +0300

    fix task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = Number(process.argv[2]);
-if (isNaN(num) || num < 1) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < num; i++) {
