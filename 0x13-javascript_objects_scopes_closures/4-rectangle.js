5c497ed973b3cab09c4ae6f05ed4133512c657d8
amasin76 <amasin76@gmail.com>
2023-09-13 21:05:08 +0300

    Fix

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       [this.width, this.height] = [w, h];
     }
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
@@ -14,13 +14,13 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     if (this.width && this.height) {
       [this.width, this.height] = [this.height, this.width];
     }
   }
 
-  double() {
+  double () {
     if (this.width && this.height) {
       [this.width, this.height] = [this.width * 2, this.height * 2];
     }
